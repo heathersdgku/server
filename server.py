ea1ac9484d9d890defd9658880c3eba23dc0d50e
heathersdgku <heatherchoe@gmail.com>
2024-07-02 23:30:48 -0700

    create repo

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+import json
+
+#GLobal Vars
+items = []
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "hello from flask"
+
+
+#API endpoints
+#JSON
+# create an API to perform a get request this url: /api/about
+#return your name as a message
+
+@app.get("/api/about")
+def about():
+    me = {"name": "Heather"}
+    return json.dumps(me)
+
+@app.post("/api/products")
+def saveProducts():
+    product = request.get.json()
+    print (product)
+    #mock the save
+    items.append(product)
+    return json.dumps()
+
+
+
+@app.get("/api/products")
+def getProduct():
+    return json.dumps(items)
+
+
+app.run(debug = True)
